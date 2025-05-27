88cc5c5bdc68cd5a24e63b5012c90a11a00cec41
Debraj1723 <debraj.basak23@gmail.com>
2025-05-27 22:40:42 +0530

    routes beginning -development

--- src/app.js
+++ src/app.js
@@ -2,19 +2,25 @@ const express = require("express");
 
 const app = express();
 
-app.use("/part-1",(req,res)=>{
-    res.send("Response from part-1")
-})
-
-app.use("/part-2",(req,res)=>{
-    res.send("Response from part-2")
-})
-
-app.use((req, res) => {
-  res.send("Hello from the server");
+app.get("/user", (req, res) => {
+  res.send({ firstName: "Debraj", lastName: "Basak", type: "GET" });
 });
 
+app.post("/user", (req, res) => {
+  res.send({ firstName: "Debraj", lastName: "Basak", type: "POST" });
+});
 
+app.delete("/user", (req, res) => {
+  res.send({ firstName: "Debraj", lastName: "Basak", type: "DELETE" });
+});
+
+app.put("/user", (req, res) => {
+  res.send({ firstName: "Debraj", lastName: "Basak", type: "PUT" });
+});
+
+app.use("/test", (req, res) => {
+  res.send("Hello from the server");
+});
 
 app.listen(3000, () => {
   console.log("Server is running on port 3000...");
