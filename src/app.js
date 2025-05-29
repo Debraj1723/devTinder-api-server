973b5b9eb7fbd4cefafe83118817d7a3d2382cc2
Debraj1723 <debraj.basak23@gmail.com>
2025-05-29 23:15:53 +0530

    adding multiple route handler

--- src/app.js
+++ src/app.js
@@ -7,7 +7,12 @@ app.get("/user", (req, res) => {
 });
 
 app.post("/user", (req, res) => {
-  res.send({ firstName: "Debraj", lastName: "Basak", type: "POST" });
+  res.send({
+    firstName: "Debraj",
+    lastName: "Basak",
+    type: "POST",
+    query: req.query,
+  });
 });
 
 app.delete("/user", (req, res) => {
@@ -21,6 +26,31 @@ app.put("/user", (req, res) => {
 app.use("/test", (req, res) => {
   res.send("Hello from the server");
 });
+
+app.use(
+  "/multi-handlers",
+  (req, res) => {
+    console.log("response from first handler");
+    res.send("response from first handler");
+  },
+  (req, res) => {
+    console.log("response from second handler");
+    res.send("response from second handler");
+  }
+);
+
+app.use(
+  "/multi-handlers-second",
+  (req, res, next) => {
+    console.log("response from first handler");
+    next();
+    res.send("response from first handler");
+  },
+  (req, res) => {
+    console.log("response from second handler");
+    res.send("response from second handler");
+  }
+);
 
 app.listen(3000, () => {
   console.log("Server is running on port 3000...");
