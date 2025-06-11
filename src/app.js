9f984be3d6fefe9c80453bf5d48ee2ddb1e628b2
Debraj1723 <debraj.basak23@gmail.com>
2025-06-11 11:42:29 +0530

    data sanitization added

--- src/app.js
+++ src/app.js
@@ -9,20 +9,78 @@ app.use(express.json());
 
 app.post("/signup", async (req, res) => {
   try {
-    // we are not creating .create method now but this is equivalent to it as we are creating an instance before saving it 
+    // we are not creating .create method now but this is equivalent to it as we are creating an instance before saving it
     const user = new User(req.body);
     await user.save();
     res.status(200).json({ message: "User saved successfully" });
   } catch (err) {
-    res.status(400).status({ message: err.message });
+    res.status(400).send({ message: err.message });
+  }
+});
+
+app.get("/user", async (req, res) => {
+  try {
+    const userEmail = req.body.email;
+    const userDetails = await User.find({ email: userEmail });
+    res.status(200).send(userDetails);
+  } catch (e) {
+    res.status(404).send("Something went wrong");
+  }
+});
+
+app.delete("/user", async (req, res) => {
+  try {
+    console.log("asdasd");
+    const userID = req.body.user;
+    await User.findByIdAndDelete(userID);
+    res.status(200).send("User deleted successfully.");
+  } catch (e) {
+    res.send(404).send("Something went wrong.");
+  }
+});
+
+app.get("/feed", async (req, res) => {
+  try {
+    const feed = await User.find({});
+    res.status(200).json(feed);
+  } catch (e) {
+    res.send(404).send("Something went wrong.");
+  }
+});
+
+app.patch("/user", async (req, res) => {
+  try {
+    const userID = req.body._id;
+    const userBody = req.body;
+    const ALLOWED_UPDATES = [
+      "_id",
+      "photoUrl",
+      "about",
+      "gender",
+      "age",
+      "skills",
+    ];
+    const isUpdateAllowed = Object.keys(userBody).every((e) => {
+      return ALLOWED_UPDATES.includes(e);
+    });
+
+    if (!isUpdateAllowed) {
+      return res.status(400).send("Updation not allowed");
+    }
+    const user = await User.findByIdAndUpdate(userID, userBody, {
+      runValidators: true,
+    });
+    res.status(200).send("User has been updated successfully");
+  } catch (e) {
+    res.send(404).send("Something went wrong.");
   }
 });
 
 connectDB()
   .then(() => {
     console.log("Database connection established ...");
-    app.listen(3000, () => {
-      console.log("Server is running on port 3000...");
+    app.listen(9000, () => {
+      console.log("Server is running on port 9000...");
     });
   })
   .catch(() => console.error("Database connection failed."));
--- src/models/user.js
+++ src/models/user.js
@@ -1,24 +1,71 @@
 const mongoose = require("mongoose");
+const validator = require("validator");
 
-const userSchema = new mongoose.Schema({
-  firstName: {
-    type: String,
+const userSchema = new mongoose.Schema(
+  {
+    firstName: {
+      type: String,
+      required: true,
+      minLength: [4, "Please mention your original name, {VALUE} is too short"],
+      maxLength: 50,
+    },
+    lastName: {
+      type: String,
+    },
+    email: {
+      type: String,
+      required: true,
+      lowercase: true,
+      unique: true,
+      trim: true,
+      validate(value) {
+        if (!validator.isEmail(value)) {
+          throw new Error("Given email ID is invalid");
+        }
+      },
+    },
+    password: {
+      type: String,
+      required: true,
+      validate: function (value) {
+        if (!validator.isStrongPassword(value)) {
+          throw new Error(
+            "Weak password :( . Your pass should include { minLength: 8, minLowercase: 1, minUppercase: 1, minNumbers: 1, minSymbols: 1, returnScore: false, pointsPerUnique: 1, pointsPerRepeat: 0.5, pointsForContainingLower: 10, pointsForContainingUpper: 10, pointsForContainingNumber: 10, pointsForContainingSymbol: 10 }"
+          );
+        }
+      },
+    },
+    age: {
+      type: Number,
+      min: 18,
+    },
+    gender: {
+      type: String,
+      validate(value) {
+        if (!["Male", "Female", "Others"].includes(value)) {
+          throw new Error("Gender data is not valid");
+        }
+      },
+    },
+    photoUrl: {
+      type: String,
+      validate(value) {
+        if (!validator.isURL(value)) {
+          throw new Error("Given string is an invalid url");
+        }
+      },
+      default:
+        "https://cdni.iconscout.com/illustration/premium/thumb/male-user-image-illustration-download-in-svg-png-gif-file-formats--person-picture-profile-business-pack-illustrations-6515860.png",
+    },
+    about: {
+      type: String,
+      default: "Hey there i am using dev tinder",
+    },
+    skills: {
+      type: [String],
+    },
   },
-  lastName: {
-    type: String,
-  },
-  email: {
-    type: String,
-  },
-  password: {
-    type: String,
-  },
-  age: {
-    type: Number,
-  },
-  gender: {
-    type: String,
-  },
-});
+  { timestamps: true }
+);
 
 module.exports = mongoose.model("User", userSchema);
